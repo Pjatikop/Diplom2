9a3680cdc2a2b45c73003a4a7d167e88c817d1bf
Pjatikop <Pjatikop@yandex.ru>
2023-07-23 18:58:06 +0300

    23.07

--- YoMA/YoMA/projects/static/uikit/app.js
+++ YoMA/YoMA/projects/static/uikit/app.js
@@ -1,7 +1,15 @@
 let alertWrapper = document.querySelector('.alert');
 let alertClose = document.querySelector('.alert__close');
 
+//if (alertWrapper) {
+//  alertClose.addEventListener('click', () =>
+//    alertWrapper.style.display = 'none'
+//)}
+
 if (alertWrapper) {
-  alertClose.addEventListener('click', () => 
-    alertWrapper.style.display = 'none'
+    for(let i=0; i < alertClose.length; i++) {
+  alertClose[i].addEventListener('click', function() {
+    this.parentNode.remove();
+    })
+    }
 )}
--- YoMA/YoMA/users/views.py
+++ YoMA/YoMA/users/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('account')
+    if request.method == 'POST':
+        username = request.POST['username'].lower()
+        password = request.POST['password']
+        try:
+            user = User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            messages.error(request, 'Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('account')
+        else:
+            messages.error(request, 'Username or password is incorrect')
+
+    return render(request, 'users/login_register.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.info(request, 'User was logged out!')
+    return redirect('login')
+
+
+def register_user(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created!')
+            login(request, user)
+            return redirect('account')
+        else:
+            messages.error(request, 'An error has occurred during registration')
+
+    context = {
+        'page': page,
+        'form': form,
+    }
+    return render(request, 'users/login_register.html', context)
+
+@login_required(login_url='login')
+def user_account(request):
+    prof = request.user.profile
+    context = {
+        'profile': prof,
+    }
+    return render(request, 'users/account.html', context)
+
